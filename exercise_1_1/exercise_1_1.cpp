6cef1871da1b203be8b68481541de79b092a1939
Ankith Kodali <ankith.kodali@gmail.com>
2020-09-23 19:27:15 +0000

    Completed Exercise 1.1

--- exercise_1_1/exercise_1_1.cpp
+++ exercise_1_1/exercise_1_1.cpp
@@ -6,9 +6,14 @@ int main() {
 	std::cout << "Use the wasd keys and enter to input a direction, or the E key to stop" << std::endl;
 
     // Write your code for step 1 here
+    std::vector<float> wheel_speeds(4);
 
     // Write your code for step 2 here
-   
+    wheel_speeds[0]=0;
+    wheel_speeds[1]=0;
+    wheel_speeds[2]=0;
+    wheel_speeds[3]=0;
+	      
     // Do not edit the next 5 lines
     bool keep_going = true;
     char direction;
@@ -16,8 +21,43 @@ int main() {
     while (keep_going) {
         std::cin >> direction; //At the begining of every iteration look for an input from the user
         // Write rest of code here (within while loop)
-
+	if (direction=='w'){
+	    for (int i=0; i<4; i++){
+		wheel_speeds[i]=1;
+	}
+	}
+	else if (direction=='a'){
+	    wheel_speeds[0]=0.5;
+	    wheel_speeds[1]=1;
+	    wheel_speeds[2]=0.5;
+	    wheel_speeds[3]=1;
+	
+	}
+	else if (direction=='s'){
+	    for (int i=0; i<4; i++){
+		wheel_speeds[i]=-1;
+	}
+	}
+	else if (direction=='d'){
+	    wheel_speeds[0]=1;
+	    wheel_speeds[1]=0.5;
+	    wheel_speeds[2]=1;
+	    wheel_speeds[3]=0.5;
+	}
+	else if (direction=='e'){
+	    for (int i=0; i<4; i++){
+	    wheel_speeds[i]=0;
+	}
+	    std::cout<<"Shutting Down"<<std::endl;
+	    bool keep_going=false;
+	}
+	else {
+	    std::cout<<"Sorry I didn't get that. Try again."<<std::endl;
+	}
+	for (int i=0; i<4; i++){
+	    std::cout<<"Wheel at index "<< i <<" has speed: "<< wheel_speeds[i] <<std::endl;
+	}
         //
     }
-
+return 0;
 }
