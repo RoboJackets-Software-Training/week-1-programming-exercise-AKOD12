daa60001a2ce279a759c56294a1d93ae205337f5
Ankith Kodali <ankith.kodali@gmail.com>
2020-09-23 20:17:42 +0000

    Completed Exercise1.1

--- exercise_1_1/exercise_1_1.cpp
+++ exercise_1_1/exercise_1_1.cpp
@@ -48,7 +48,7 @@ int main() {
 	    for (int i=0; i<4; i++){
 	    wheel_speeds[i]=0;
 	}
-	    std::cout<<"Shutting Down"<<std::endl;
+	    std::cout<<"Shutting down"<<std::endl;
 	    keep_going=false;
 	}
 	else {
